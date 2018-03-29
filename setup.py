a52c9e3433ab74ab3fb8049fe8cf07445ab43293
soerenwolfers <soeren.wolfers@gmail.com>
2018-03-29 11:54:13 +0300

    Fix dependency

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup, find_packages
 
 setup(name='swutil',
-      version='1.1.2',
+      version='1.1.3',
       description='Provide common code for validation, multiprocessing, logging, decorator, configuration, plotting, etc.',
       long_description=open('README.rst').read(),
       author='Soeren Wolfers',
       author_email='soeren.wolfers@gmail.com',
       url = 'https://bitbucket.org/soerenwolfers/swutil',
       packages=find_packages(exclude=['*tests']),#,'examples*']),
-      install_requires=['numpy','matplotlib','matplotlib2tikz','memory_profiler','multiprocessing']
+      install_requires=['numpy','matplotlib','matplotlib2tikz','memory_profiler']
 )
