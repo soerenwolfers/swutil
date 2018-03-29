46d8169d797e232bf84683af63114c82874e6928
soerenwolfers <soeren.wolfers@gmail.com>
2018-03-29 11:48:42 +0300

    Version 1.1.2
    Add peak memory tracking and timer context manager; rename multiprocessing to avoid conflicts; fix setup.py

--- swutil/decorators.py
+++ swutil/decorators.py
@@ -4,6 +4,7 @@ from _io import StringIO
 import inspect
 import collections
 
+
 def log_calls(function):
     '''
     Decorator that logs function calls in their self.log
@@ -171,10 +172,52 @@ def print_runtime(function):
         ps.sort_stats('cumulative').print_stats(20)
         return output
     return wrapper
-   
 
+def print_peak_memory(func,stream = None):
+    """
+    Print peak memory usage (in MB) of a function call
+    
+    :param func: Function to be called
+    :param stream: Stream to write peak memory usage (defaults to stdout)  
+    
+    https://stackoverflow.com/questions/9850995/tracking-maximum-memory-usage-by-a-python-function
+    """
+    import time
+    import psutil
+    import os
+    memory_denominator=1024**2
+    memory_usage_refresh=0.05
+    def wrapper(*args,**kwargs):
+        from multiprocessing.pool import ThreadPool
+        pool = ThreadPool(processes=1)
+        process = psutil.Process(os.getpid())
+        start_mem = process.memory_info().rss
+        delta_mem = 0
+        max_memory = 0
+        async_result = pool.apply_async(func, args,kwargs)
+        # do some other stuff in the main process
+        while(not async_result.ready()):
+            current_mem = process.memory_info().rss
+            delta_mem = current_mem - start_mem
+            if delta_mem > max_memory:
+                max_memory = delta_mem
+            # Check to see if the library call is complete
+            time.sleep(memory_usage_refresh)
+        
+        return_val = async_result.get()  # get the return value from your function.
+        max_memory /= memory_denominator
+        if stream is not None:
+            stream.write(str(max_memory))
+        return return_val
+    return wrapper
 
 if __name__=='__main__':
+    import numpy as np
+    @print_peak_memory
+    def f(x):
+        return np.mean(np.random.rand(1000000000))
+    print(f(3))
+
 
     @declaration
     def read_out(a,loudness='loudly',c=1):
--- swutil/time.py
+++ swutil/time.py
@@ -1,8 +1,10 @@
-'''
-time.sleep() substitute 
-'''
+import timeit
+from matplotlib import pyplot
+from swutil.plots import plot_convergence
+
 def snooze(value):
     '''
+    time.sleep() substitute
     Keep busy for some time (very roughly and depending on machine value is ms)
     :param value: Time. Actual busy time depends on machine
     :type value: Number
@@ -10,3 +12,12 @@ def snooze(value):
     for i in range(int(0.65e3 * value)):
         __ = 2 ** (i / value)
     return 0
+
+class Timer():
+    def __enter__(self,name=''):
+        self.name = name
+        self.start_time = timeit.default_timer()
+    def __exit__(self,*args):
+        elapsed = timeit.default_timer()-self.start_time
+        print('Elapsed time ({}): {.3f}s'.format(self.name,elapsed))
+
