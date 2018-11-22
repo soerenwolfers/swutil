55a7d6202b64f35737ef4f0ac3f478f7d85b9e21
Soeren Wolfers <soeren.wolfers@gmail.com>
2018-11-22 11:13:54 +0300

    Version 1.3

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup, find_packages
 
 setup(name='swutil',
-      version='1.2.1',
+      version='1.3',
       description='Provide common code for validation, multiprocessing, logging, decorator, configuration, plotting, etc.',
       long_description=open('README.rst').read(),
       author='Soeren Wolfers',
       author_email='soeren.wolfers@gmail.com',
-      url = 'https://bitbucket.org/soerenwolfers/swutil',
+      url = 'https://github.com/soerenwolfers/swutil',
       packages=find_packages(exclude=['*tests']),#,'examples*']),
       install_requires=['numpy','matplotlib','matplotlib2tikz','memory_profiler','pathos','dill','PyPDF2']
 )
