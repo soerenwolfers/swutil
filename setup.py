3d9b894adbb00b74688b0744ea98c8a13648f88a
Soeren Wolfers <soeren.wolfers@gmail.com>
2019-05-10 20:55:44 +0300

    Version 1.3.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='swutil',
-      version='1.3.2',
+      version='1.3.3',
       description='Provide common code for validation, multiprocessing, logging, decorator, configuration, plotting, etc.',
       long_description=open('README.rst').read(),
       author='Soeren Wolfers',
