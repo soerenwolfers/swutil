fb721f17b7659fe05393dc8ce233e4eae1b31112
Soeren Wolfers <soeren.wolfers@gmail.com>
2019-07-28 19:10:11 +0300

    Try to install readline

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
+import warnings
+from setuptools.command.install import install 
+
+class PostInstallCommand(install):
+    def run(self):
+        try:
+            from pip._internal import main as pipmain
+            pipmain(['install', 'readline'])
+        except:
+            warnings.warn('Could not install readline')
+        install.run(self)
 
 setup(name='swutil',
-      version='1.4.2',
+      version='1.4.3',
       python_requires='>=3.6',
       description='Provide common code for validation, multiprocessing, logging, decorator, configuration, plotting, etc.',
       long_description=open('README.rst').read(),
@@ -10,10 +21,6 @@ setup(name='swutil',
       author_email='soeren.wolfers@gmail.com',
       url = 'https://github.com/soerenwolfers/swutil',
       packages=find_packages(exclude=['*tests']),#,'examples*']),
+      cmdclass={ 'install': PostInstallCommand, },
       install_requires=['numpy','matplotlib','matplotlib2tikz','memory_profiler','pathos','dill','PyPDF2']
 )
-try:
-    from pip._internal import main as pipmain
-    pipmain(['install', 'readline'])
-except:
-    warnings.warn('Could not install readline')
